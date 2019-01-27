7cd6bd2bcad5d1af333ec3c1fcc3f96083e2a93f
nikolehto <niko.lehto@mbnet.fi>
2019-01-27 17:53:53 +0200

    add vt3 start

---
+++ vt3/intelement.h
@@ -0,0 +1,37 @@
+#ifndef INTELEMENT_H
+#define INTELEMENT_H
+
+#include <sstream>
+#include "catch.hpp"
+
+/**
+ * @file intelement.h
+ * @version 1.0
+ * @brief Declaration of IntElement
+ * @author Niko Lehto
+ */
+
+class IntElement
+{
+private:
+	int intelement;
+
+public: 
+	IntElement();
+	IntElement(int v);
+    IntElement(const std::string& number);
+	IntElement(const IntElement& i);
+	~IntElement();
+	int getVal() const;
+	void setVal(int v);
+	
+	IntElement& operator+=(const IntElement& i);
+	IntElement& operator-=(const IntElement& i);
+	IntElement& operator*=(const IntElement& i);
+	friend bool operator==(const IntElement& a, const IntElement& b);
+	friend IntElement operator+(const IntElement& a, const IntElement& b);
+	friend IntElement operator-(const IntElement& a, const IntElement& b);
+	friend IntElement operator*(const IntElement& a, const IntElement& b);
+	friend std::ostream& operator<<(std::ostream& o, const IntElement& v);
+};
+#endif
