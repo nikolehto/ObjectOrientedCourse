1a20394b9b10a9c36de200981e9faa2d32dff047
nikolehto <niko.lehto@mbnet.fi>
2019-02-02 00:07:09 +0200

    intelement and test working

---
+++ vt4/intelement.h
@@ -0,0 +1,40 @@
+#ifndef INTELEMENT_H
+#define INTELEMENT_H
+
+#include <sstream>
+#include "catch.hpp"
+
+/**
+ * @file intelement.h
+ * @version 1.0
+ * @brief Declaration of IntElement
+ * @author Niko Lehto
+ */
+
+class IntElement
+{
+private:
+	int intelement;
+
+public:
+	IntElement();
+    IntElement(const std::string& number);
+	IntElement(int v);
+	IntElement(const IntElement& i);
+	~IntElement();
+	int getVal() const;
+	std::unique_ptr<IntElement> clone() const;
+	void setVal(int v);
+
+
+	IntElement& operator+=(const IntElement& i);
+	IntElement& operator-=(const IntElement& i);
+	IntElement& operator*=(const IntElement& i);
+	bool operator==(const IntElement& i) const;
+
+	friend IntElement operator+(const IntElement& a, const IntElement& b);
+	friend IntElement operator-(const IntElement& a, const IntElement& b);
+	friend IntElement operator*(const IntElement& a, const IntElement& b);
+	friend std::ostream& operator<<(std::ostream& o, const IntElement& v);
+};
+#endif
---
+++ vt4/squarematrix.h
@@ -0,0 +1,42 @@
+#ifndef SQUAREMATRIX_H
+#define SQUAREMATRIX_H
+
+#include "intelement.h"
+#include "catch.hpp"
+#include <sstream>
+
+
+/**
+ * @file squarematrix.h
+ * @version 2.0
+ * @brief Declaration of SquareMatrix
+ * @author Niko Lehto
+ */
+class SquareMatrix
+{
+private:
+	int n;
+	std::vector<std::vector<IntElement>> elements;
+	void fromString(const std::string& s);
+
+public:
+	SquareMatrix();
+	SquareMatrix(const std::string& s);
+	SquareMatrix(const SquareMatrix& m);
+	~SquareMatrix();
+
+	void print(std::ostream& os) const;
+	std::string toString() const;
+	SquareMatrix transpose() const;
+
+	bool operator==(const SquareMatrix& m) const;
+	SquareMatrix& operator=(const SquareMatrix& m);
+	SquareMatrix& operator+=(const SquareMatrix& m);
+	SquareMatrix& operator-=(const SquareMatrix& m);
+	SquareMatrix& operator*=(const SquareMatrix& m);
+	friend SquareMatrix operator+(const SquareMatrix& a, const SquareMatrix& b);
+	friend SquareMatrix operator-(const SquareMatrix& a, const SquareMatrix& b);
+	friend SquareMatrix operator*(const SquareMatrix& a, const SquareMatrix& b);
+	friend std::ostream& operator<<(std::ostream& stream, const SquareMatrix& m);
+};
+#endif
